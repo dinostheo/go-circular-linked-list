da34ab4ff179f25e990496b3d63b99ca0bb79f56
Dinos Theodorou <dinos@crowdynews.com>
2017-11-10 10:34:47 +0100

    Implement the circular linked list interface

--- circular-linked-list.go
+++ circular-linked-list.go
@@ -6,15 +6,122 @@ type Node struct {
 }
 
 type CircularLinkedList struct {
-	head, tail *Node
-	len        int
+	head, tail, current *Node
+	len                 int
 }
 
-/*
- - AddPrevious() *Node
- - AddNext() *Node
- - Previous() *Node
- - Next() *Node
- - Remove() *Node
- - Length() int
-*/
+func (c *CircularLinkedList) addInit(n *Node) {
+	c.head = n
+	c.tail = n
+	c.current = n
+}
+
+func (c *CircularLinkedList) Prepend(n *Node) *Node {
+	if c.len == 0 {
+		c.addInit(n)
+	} else {
+		c.head.Previous = n
+		c.tail.Next = n
+		n.Next = c.head
+		n.Previous = c.tail
+		c.head = n
+		c.current = n
+	}
+
+	c.len++
+
+	return c.current
+}
+
+func (c *CircularLinkedList) Append(n *Node) *Node {
+	if c.len == 0 {
+		c.addInit(n)
+	} else {
+		c.head.Previous = n
+		c.tail.Next = n
+		n.Next = c.head
+		n.Previous = c.tail
+		c.tail = n
+		c.current = n
+	}
+
+	c.len++
+
+	return c.current
+}
+
+func (c *CircularLinkedList) Length() int {
+	return c.len
+}
+
+func (c *CircularLinkedList) Current() *Node {
+	if c.current != nil {
+		return c.current
+	}
+
+	return c.head
+}
+
+func (c *CircularLinkedList) Next() *Node {
+	if c.current != nil {
+		c.current = c.current.Next
+		return c.current
+	}
+
+	return nil
+}
+
+func (c *CircularLinkedList) Previous() *Node {
+	if c.current != nil {
+		c.current = c.current.Previous
+		return c.current
+	}
+
+	return nil
+}
+
+func (c *CircularLinkedList) AddBefore(n *Node) *Node {
+	if c.len == 0 {
+		c.addInit(n)
+	} else {
+		c.current.Previous.Next = n
+		c.current.Previous = n
+		n.Previous = c.current.Previous
+		n.Next = c.current
+		c.current = n
+	}
+
+	c.len++
+
+	return c.current
+}
+
+func (c *CircularLinkedList) AddAfter(n *Node) *Node {
+	if c.len == 0 {
+		c.addInit(n)
+	} else {
+		c.current.Next.Previous = n
+		c.current.Next = n
+		n.Previous = c.current
+		n.Next = c.current.Next
+		c.current = n
+	}
+
+	c.len++
+
+	return c.current
+}
+
+func (c *CircularLinkedList) Remove() *Node {
+	if c.len == 0 {
+		return nil
+	}
+
+	c.current.Previous.Next = c.current.Next
+	c.current.Next.Previous = c.current.Previous
+
+	c.current = c.current.Next
+	c.len--
+
+	return c.current
+}
